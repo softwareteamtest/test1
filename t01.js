1773bc95e2bb9f329b2e7023e84efcb409554851
kidxxid <3267982421@qq.com>
2022-11-19 16:24:06 +0800

    js

--- t01.js
+++ t01.js
@@ -2,15 +2,16 @@ function submit(){
     $.ajax({
         url:"",
         type:"post",
-        dataType:"json",
-        data:JSON.stringify({
-                Number:   $("#Num").val(),
-            }),
+        dataType:"text",
+        data:{
+                Number: $("#Num").val(),
+            },
         success:function (data){
             console.log("传送成功")
             console.log(data)
             console.log(data.status)
             console.log(data.time)
+            alert(data)
         },
         error:function (){
             alert("传送失败")
