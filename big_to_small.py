cfb9fc5db7377c018505b94fa2a1759b3eace343
asdj78 <112798302+asdj78@users.noreply.github.com>
2022-11-19 16:55:15 +0800

    Update big_to_small.py

--- big_to_small.py
+++ big_to_small.py
@@ -1,7 +1,7 @@
 
-    def big_to_small(self, big):
-        change_int = ''
-        change_int = self.change_big(big[:-1])
+    def big_to_small(self, big):  ##大写转小写
+        change_int = '' 
+        change_int = self.change_big(big[:-1]) ##去掉大写末尾的元整
         remove = 0
         for s in change_int:
             if s != '0':
