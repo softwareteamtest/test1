b1f8035800377ac464d23f0a04077db7e1682128
kidxxid <3267982421@qq.com>
2022-11-19 15:28:58 +0800

    2

---
+++ t01.js
@@ -0,0 +1,22 @@
+function submit(){
+    $.ajax({
+        url:"",
+        type:"post",
+        dataType:"json",
+        data:JSON.stringify({
+                Number:   $("#Num").val(),
+            }),
+        success:function (data){
+            console.log("传送成功")
+            console.log(data)
+            console.log(data.status)
+            console.log(data.time)
+        },
+        error:function (){
+            alert("登录失败")
+        }
+    })
+}
+function reset(){
+    document.getElementById("myForm").reset()
+}
